b69da69c8c9984baf12e9e24f3305d24ddf5c919
masahosono <dev.masahosono@gmail.com>
2022-03-13 00:40:11 +0900

    rm unused env

--- types/global.d.ts
+++ types/global.d.ts
@@ -2,8 +2,6 @@ declare namespace NodeJS {
     interface ProcessEnv {
         readonly NODE_ENV: 'development' | 'production';
         readonly PORT: number;
-        readonly SITE_URL: string;
-        readonly SITE_LOCALHOST_URL: string;
         readonly SITE_NAME: string;
         readonly SITE_DEFAULT_DESCRIPTION: string;
         readonly ARTICLE_PER_PAGE: number;
