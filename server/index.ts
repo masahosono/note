35b1211386b9e1c954457dfdd01e6520a40cfc1d
masahosono <dev.masahosono@gmail.com>
2022-03-14 21:03:39 +0900

    起動ポートを3001に修正

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,6 @@ import express from 'express'
 import next from 'next'
 
 const dev = process.env.NODE_ENV !== 'production'
-const port = process.env.PORT || 3000;
 
 const app = next({ dev })
 const handler = app.getRequestHandler()
@@ -10,15 +9,12 @@ const server = express()
 
 app.prepare()
     .then(() => {
-        server.use(
-            express.static('public'))
-
         server.all('*', (req, res) => {
             return handler(req, res)
         })
 
-        server.listen(port, () => {
-            console.debug(`> Ready on http://localhost:${port}`)
+        server.listen(3001, () => {
+            console.debug(`> Ready on http://localhost:3001`)
         })
     })
     .catch(() => {
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,7 +1,6 @@
 declare namespace NodeJS {
     interface ProcessEnv {
         readonly NODE_ENV: 'development' | 'production';
-        readonly PORT: number;
         readonly SITE_NAME: string;
         readonly SITE_DEFAULT_DESCRIPTION: string;
         readonly ARTICLE_PER_PAGE: number;
